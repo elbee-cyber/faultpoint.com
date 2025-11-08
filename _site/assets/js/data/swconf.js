7d72bbe27af541fb79c93359119ca15980a62315
elbee-cyber <elbee_ez@protonmail.com>
2025-11-08 18:39:58 -0500

    Publish static site to /docs

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1762634074',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1762645198',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
