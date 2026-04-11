dc37ab14826e7e24a9dc9ceca22bfedfe20ce247
elbee-cyber <elbee_ez@protonmail.com>
2026-04-11 18:37:34 -0400

    Publish static site to /docs

---
+++ docs/assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf = { cacheName: 'chirpy-1775947053',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', '/files/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
